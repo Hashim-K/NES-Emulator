c7e982f3acc58091b7a21d808f8194c78b74d075
Friso Smit <fw.smit01@gmail.com>
2024-10-03 15:07:58 +0200

    Move cpu to cpu/mod.rs

---
+++ src/cpu/mod.rs
@@ -0,0 +1,42 @@
+use tudelft_nes_test::TestableCpu;
+use crate::error::{MyTickError, MyGetCpuError};
+use tudelft_nes_ppu::{Cpu, Ppu};
+
+pub struct MyCpu {}
+
+/// See docs of `Cpu` for explanations of each function
+impl Cpu for MyCpu {
+    type TickError = MyTickError;
+
+    fn tick(&mut self, _ppu: &mut Ppu) -> Result<(), MyTickError> {
+        todo!()
+    }
+
+    fn ppu_read_chr_rom(&self, _offset: u16) -> u8 {
+        todo!()
+    }
+
+    fn non_maskable_interrupt(&mut self) {
+        todo!()
+    }
+}
+
+/// Implementing this trait allows automated tests to be run on your cpu.
+/// The crate `tudelft-nes-test` contains all kinds of small and large scale
+/// tests to find bugs in your cpu.
+impl TestableCpu for MyCpu {
+    type GetCpuError = MyGetCpuError;
+
+    fn get_cpu(_rom: &[u8]) -> Result<Self, MyGetCpuError> {
+        todo!()
+    }
+
+    fn set_program_counter(&mut self, _value: u16) {
+        todo!()
+    }
+
+    fn memory_read(&self, _address: u16) -> u8 {
+        todo!()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,9 @@
 use log::LevelFilter;
-use tudelft_nes_ppu::{run_cpu, Cpu, Mirroring, Ppu};
-use tudelft_nes_test::TestableCpu;
-use error::{MyTickError, MyGetCpuError};
+use tudelft_nes_ppu::{run_cpu, Mirroring};
+use cpu::MyCpu;
 
+mod cpu;
 mod error;
-
-pub struct MyCpu {}
-
-/// See docs of `Cpu` for explanations of each function
-impl Cpu for MyCpu {
-    type TickError = MyTickError;
-
-    fn tick(&mut self, _ppu: &mut Ppu) -> Result<(), MyTickError> {
-        todo!()
-    }
-
-    fn ppu_read_chr_rom(&self, _offset: u16) -> u8 {
-        todo!()
-    }
-
-    fn non_maskable_interrupt(&mut self) {
-        todo!()
-    }
-}
-
-/// Implementing this trait allows automated tests to be run on your cpu.
-/// The crate `tudelft-nes-test` contains all kinds of small and large scale
-/// tests to find bugs in your cpu.
-impl TestableCpu for MyCpu {
-    type GetCpuError = MyGetCpuError;
-
-    fn get_cpu(_rom: &[u8]) -> Result<Self, MyGetCpuError> {
-        todo!()
-    }
-
-    fn set_program_counter(&mut self, _value: u16) {
-        todo!()
-    }
-
-    fn memory_read(&self, _address: u16) -> u8 {
-        todo!()
-    }
-}
 
 fn main() {
     env_logger::builder().filter_level(LevelFilter::Info).init();
@@ -54,9 +16,9 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::MyCpu;
     use log::LevelFilter;
     use tudelft_nes_test::{run_tests, TestSelector};
+    use crate::cpu::MyCpu;
 
     /// This test fails in the template, since you didn't implement the cpu yet.
     #[test]
