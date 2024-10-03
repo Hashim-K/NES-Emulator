791a2c3be52acf5d1a0abc3231bf3e1fae2985d1
Friso Smit <fw.smit01@gmail.com>
2024-10-03 16:30:08 +0200

    Implement ines header parsing
    Fixes #1

--- src/cartridge/nrom.rs
+++ src/cartridge/nrom.rs
@@ -1,14 +1,8 @@
 
 struct nrom {
-    charactor_memory_size: usize,
     character_memory_writable: bool,
     character_memory: [u8],
 
-    program_rom_size: usize,
     program_rom: [u8],
-
-    mirroring: bool,
-    peristent_memory: bool,
-    ignore_mirroring_control: bool,
 
 }
