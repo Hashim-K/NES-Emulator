df978f8d00c15ccaacf03bd7098f448f77fe1112
Friso Smit <fw.smit01@gmail.com>
2024-10-15 15:17:27 +0200

    Implement ROM reading from file
    Fixes #16

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use cpu::MyCpu;
 use error::MainError;
 use log::LevelFilter;
+use std::env;
+use std::fs;
 use std::process::ExitCode;
 use tudelft_nes_ppu::{run_cpu, Mirroring};
 use tudelft_nes_test::TestableCpu;
@@ -10,10 +12,10 @@ mod cpu;
 mod error;
 mod memory;
 
-fn run() -> Result<(), MainError> {
+fn run(file_bytes: &[u8]) -> Result<(), MainError> {
     env_logger::builder().filter_level(LevelFilter::Info).init();
 
-    let cpu = MyCpu::get_cpu(ROM_NROM_TEST)?;
+    let cpu = MyCpu::get_cpu(file_bytes)?;
 
     log::info!("running cpu");
     run_cpu(cpu, Mirroring::Horizontal);
@@ -21,7 +23,20 @@ fn run() -> Result<(), MainError> {
 }
 
 fn main() -> ExitCode {
-    match run() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        eprintln!("Invalid number of arguments");
+        return ExitCode::from(2);
+    }
+
+    let file_bytes = if args.len() == 2 {
+        fs::read(&args[1]).unwrap()
+    } else {
+        ROM_NROM_TEST.to_vec()
+    };
+
+    match run(&file_bytes) {
         Ok(_) => ExitCode::SUCCESS,
         Err(a) => {
             eprintln!("{}", a);
