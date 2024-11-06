cc80a813978b566a6c13458879ff49cdf3dc0197
Friso Smit <fw.smit01@gmail.com>
2024-11-06 22:56:51 +0100

    Add comments for the Controller struct

--- src/memory/controller.rs
+++ src/memory/controller.rs
@@ -2,17 +2,22 @@ use tudelft_nes_ppu::{Buttons, Ppu};
 
 #[derive(Debug)]
 pub struct Controller {
-    strobe: bool,
-    buttons: Buttons,
-    read_index: u8,
+    strobe: bool,     // The least significant bit that is written to the controller
+    buttons: Buttons, // Holds the button state from when strobe was last high
+    read_index: u8,   // Index of the button being read
 }
 
 impl Controller {
+    // Writes to the controller input.
+    //
+    // Should be mapped to address $4016
+    // Only the least significant bit of the input will be used.
     pub fn write(&mut self, byte: u8, ppu: &Ppu) {
         self.strobe = (byte & 0b1) == 1;
         self.clock_pulse(ppu);
     }
 
+    // Refreshes the buttons when strobe is high. This should be called every clock cycle.
     pub fn clock_pulse(&mut self, ppu: &Ppu) {
         if self.strobe {
             self.buttons = ppu.get_joypad_state();
@@ -20,6 +25,9 @@ impl Controller {
         }
     }
 
+    // Returns the current button's value
+    //
+    // Should be mapped to address $4016 for controller 1 and $4017 for controller 2
     pub fn read(&mut self, ppu: &Ppu) -> u8 {
         let result = u8::from(match self.read_index {
             0 => self.buttons.a,
